c34e18b021c2ba969f956ccd07d5fd94326d939b
Agnieszka Zalewska <agnieszka.e.zalewska@gmail.com>
2021-04-23 21:56:35 +0200

    update

--- examples/10-basic-api/app.js
+++ examples/10-basic-api/app.js
@@ -1,28 +1,28 @@
-const result = document.querySelector('.result')
+const result = document.querySelector('.result');
 
 const fetchData = async () => {
   try {
-    const { data } = await axios.get('/api/2-basic-api')
+    const { data } = await axios.get('/api/10-basic-api');
     const products = data
       .map((product) => {
         const {
           image: { url },
           name,
           price,
-        } = product
+        } = product;
         return `<article class="product">
      <img src="${url}" alt="${name}"/>
      <div class="info">
 <h5>${name}</h5>
 <h5 class="price">$${price}</h5>
      </div>
-     </article>`
+     </article>`;
       })
-      .join('')
-    result.innerHTML = products
+      .join('');
+    result.innerHTML = products;
   } catch (error) {
-    result.innerHTML = `<h4>There was an error. Please try again later</h4>`
+    result.innerHTML = `<h4>There was an error. Please try again later</h4>`;
   }
-}
+};
 
-fetchData()
+fetchData();
