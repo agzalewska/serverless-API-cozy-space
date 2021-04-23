595bb1c32bcb0c4c1a5471523edfb42fc7041011
Agnieszka Zalewska <agnieszka.e.zalewska@gmail.com>
2021-04-23 15:12:13 +0200

    new asset dataUrl.js

--- assets/dataUrl.js
+++ assets/dataUrl.js
@@ -2,7 +2,7 @@ const items = [
   {
     id: 'rec7P3XUwcf8Ny6D8',
     name: 'Hallie Lounge Chair',
-    image:
+    url:
       'https://dl.airtable.com/.attachments/741104c782a7e45b3281abb65d82a6c3/5f587020/cozyspace.JPG',
     price: 575,
     category: 'Bedroom',
@@ -16,7 +16,7 @@ const items = [
   {
     id: 'recYEc7x36vJolPm9',
     name: 'Hallie Lounge Chair',
-    image:
+    url:
       'https://dl.airtable.com/.attachments/a2d199db5d52ec61e2c5477d99612eeb/44b95555/0520088310032_014_d.jpg',
     price: 575,
     category: 'Bedroom',
@@ -30,7 +30,7 @@ const items = [
   {
     id: 'recZVZjRO213YdOE9',
     name: 'Hallie Lounge Chair',
-    image:
+    url:
       'https://dl.airtable.com/.attachments/04cc15d60b1f288638c696a066ad64ac/d11c7b4c/0520088310032_014_d.jpg',
     price: 575,
     category: 'Bedroom',
@@ -44,7 +44,7 @@ const items = [
   {
     id: 'recpr22Hq2hp5e4B6',
     name: 'Hallie Lounge Chair',
-    image:
+    url:
       'https://dl.airtable.com/.attachments/ec50a5b2a073f8aca1929c80f6818448/c4166139/6bfb39700e9bb834332103b4ffb1e37f.jpg',
     price: 575,
     category: 'Living Room',
